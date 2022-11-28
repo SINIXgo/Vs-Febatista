74980d9d0b44f87926d07f7a6dc7ebabd940fc41
SINIXgo <116418436+SINIXgo@users.noreply.github.com>
2022-11-28 11:35:39 -0300

    Add files via upload

---
+++ assets/preload/stages/escritório.lua
@@ -0,0 +1,11 @@
+function onCreate()
+	
+        makeLuaSprite('escritórioback','escritórioback',-600,-200);
+	addLuaSprite('escritórioback',false)
+        setLuaSpriteScrollFactor('',0.9,0.9);
+	
+	makeLuaSprite('mesa','mesa',-600,-250);
+	addLuaSprite('mesa',true)
+        setLuaSpriteScrollFactor('mesa',0.9,0.9);
+end
+	
